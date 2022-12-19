4c6a275986c3eba6597ab2463fa8153845e54ea4
Kemal Alp <kemal@qovaq.co.uk>
2022-12-19 22:41:37 +0000

    Adding click basic click features
    Adding functions for square click

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,13 +2,29 @@ import React, { useState } from 'react';
 import Board from './Board';
 
 const Game: React.FC = () => {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
-    const handleClick = () => {
+    const handleClick = (i: number) => {
+        const newSquares = squares.slice();
+
+        newSquares[i] = xIsNext ? 'X' : 'O';
+        setSquares(newSquares);
+        setXIsNext(!xIsNext);
     };
 
+    let status;
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+
     return (
-        <Board squares={squares} onClick={handleClick} />
+        <div className="game">
+            <div className="game-board">
+                <Board squares={squares} onClick={handleClick} />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+            </div>
+        </div>
     );
 };
 
